234fb377f2ef735526de74d7e1506dec641fc2bd
Abdal Shakir <64357110+abdalshakir@users.noreply.github.com>
2022-03-11 19:47:13 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {Button, Form } from 'react-bootstrap';
+import { useState } from "react";
+const axios = require("axios");
 
 function App() {
+  const [text, setText] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  function sendMessage(e) {
+    e.preventDefault();
+
+    console.log("text: ", text);
+
+    setMessages((prev) => {
+      return [{ sender: "user", text: text }, ...prev];
+    });
+
+    axios
+      .post(`http://localhost:7001/talktochatbot`, {
+        text: text,
+      })
+      .then((response) => {
+        console.log("response", response.data.text);
+
+        setMessages((prev) => {
+          return [{ sender: "bot", text: response.data.text }, ...prev];
+        });
+        e.target.reset();
+        setText("");
+      })
+      .catch((error) => {
+        console.log("error: ", error);
+
+        setMessages((prev) => {
+          return [
+            { sender: "bot", text: "dummy response from chatbot" },
+            ...prev,
+          ];
+        });
+        e.target.reset();
+        setText("");
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="bg-light w-50 m-auto p-3" style={{height: "500px"}}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        {messages?.map((eachMessage, eachMessageIndex) => (
+          <div
+            key={`${eachMessageIndex}-message`}
+            style={{
+              display: "flex",
+              justifyContent:
+                eachMessage.sender === "user" ? "flex-end" : "flex-start",
+            }}
+          >
+            <div>{eachMessage.text}</div>
+          </div>
+        ))}
+      </div>
+      <br/>
+      <Form onSubmit={sendMessage}>
+        <Form.Group
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+          className="mb-3"
+          controlId="formBasicEmail"
         >
-          Learn React
-        </a>
-      </header>
+          <Form.Control
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            type="text"
+            placeholder="Enter your message"
+          />
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+      
     </div>
   );
 }
